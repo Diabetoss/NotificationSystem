5f35337706727e5301a9955eea7c4ff4a5f50b24
Diabeto <119340925+Diabetoss@users.noreply.github.com>
2025-09-13 23:48:02 +0200

    Create Mainship.lua

---
+++ Mainship.lua
@@ -0,0 +1,20 @@
+--[[
+
+If you aint a scripter dont touch this
+
+]]--
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local NotificationEvent = ReplicatedStorage:WaitForChild("NotificationEvent2")
+
+local function createNotification(player, config)
+	NotificationEvent:FireClient(player, config)
+end
+
+NotificationEvent.OnServerEvent:Connect(function(player, config)
+	if typeof(config) == "table" then
+		createNotification(player, config)
+	else
+		warn("Invalid notification request received by user:", player.Name)
+	end
+end)
